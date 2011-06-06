467e8d40ad46dea86c58dd663d3b0a5d104e6057
Clemens Boos <clemens.boos@gmx.de>
2011-06-06 06:15:12 +0200

    Completed LaserShape

--- mod_redstoneExtended/src/net/minecraft/src/redstoneExtended/LaserShape.java
+++ mod_redstoneExtended/src/net/minecraft/src/redstoneExtended/LaserShape.java
@@ -1,13 +1,39 @@
 package net.minecraft.src.redstoneExtended;
 
+import net.minecraft.src.Block;
+import net.minecraft.src.NBTTagCompound;
+
 public class LaserShape {
-    public float width;
+    public float width = 1.0f;
 
-    public boolean collision;
+    public boolean collision = false;
 
-    public short damage;
+    public short damage = (short)0;
 
-    public byte texture;
+    public byte texture = (byte)Block.blockSnow.blockIndexInTexture;
 
+    public LaserShape() {
+    }
 
+    public LaserShape(float width, boolean collision, short damage, byte texture) {
+        this.width = width;
+        this.collision = collision;
+        this.damage = damage;
+        this.texture = texture;
+    }
+
+    public static LaserShape readFromNBT(NBTTagCompound nbtTagCompound) {
+        float width = nbtTagCompound.getFloat("Width");
+        boolean collision = nbtTagCompound.getBoolean("Collision");
+        short damage = nbtTagCompound.getShort("Damage");
+        byte texture = nbtTagCompound.getByte("Texture");
+        return new LaserShape(width, collision, damage, texture);
+    }
+
+    public static void writeToNBT(NBTTagCompound nbtTagCompound, LaserShape laserShape) {
+        nbtTagCompound.setFloat("Width", laserShape.width);
+        nbtTagCompound.setBoolean("Collision", laserShape.collision);
+        nbtTagCompound.setShort("Damage", laserShape.damage);
+        nbtTagCompound.setByte("Texture", laserShape.texture);
+    }
 }
